ca91eefac7512281971a0f89ce108e4a1c697202
Terroirsdavenir <paul.skenadje@terroirs-avenir.fr>
2020-05-29 13:13:54 +0200

    [IMPL]: commit to test odoo runbot

--- arkeup_brand/models/sale_order.py
+++ arkeup_brand/models/sale_order.py
@@ -5,29 +5,29 @@ from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
-	_inherit = 'sale.order'
+    _inherit = 'sale.order'
 
-	@api.model
-	def default_get(self, list_fields):
-		res = super(SaleOrder, self).default_get(list_fields)
-		if self.env.user.brand_ids:
-			res.update({'brand_id': self.env.user.brand_ids[0].id})
-		return res
+    @api.model
+    def default_get(self, list_fields):
+        res = super(SaleOrder, self).default_get(list_fields)
+        if self.env.user.brand_ids:
+            res.update({'brand_id': self.env.user.brand_ids[0].id})
+        return res
 
-	@api.onchange('brand_id')
-	def _onchange_brand_id(self):
-		super(SaleOrder, self)._onchange_brand_id()
-		for rec in self:
-			vals = {}
-			if rec.brand_id.team_id:
-				vals.update({'team_id': rec.brand_id.team_id})
-			if rec.brand_id.analytic_account_id:
-				vals.update({'analytic_account_id': rec.brand_id.analytic_account_id})
-			rec.update(vals)
+    @api.onchange('brand_id')
+    def _onchange_brand_id(self):
+        super(SaleOrder, self)._onchange_brand_id()
+        for rec in self:
+            vals = {}
+            if rec.brand_id.team_id:
+                vals.update({'team_id': rec.brand_id.team_id})
+            if rec.brand_id.analytic_account_id:
+                vals.update({'analytic_account_id': rec.brand_id.analytic_account_id})
+            rec.update(vals)
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     brand_id = fields.Many2one(related='order_id.brand_id', readonly=True, store=True)
-
+# TODO: just for testing odoo runbot
