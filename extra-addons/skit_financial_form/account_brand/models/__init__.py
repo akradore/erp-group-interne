c8762fee3f95e2207869d38fcc0a00ffc9265d0c
omar.ouhari@gmail.com <paul.skenadje@terroirs-avenir.fr>
2020-06-04 12:41:25 +0200

    [IMPL]: restructe the repository

--- extra-addons/skit_financial_form/account_brand/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2020 Odoo Community Association (OCA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import account_move
-from . import account_analytic_account
